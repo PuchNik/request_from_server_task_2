6744f5a19fb4b181e0922119c30207d559f73250
PuchNik <i.p.chn.k@gmail.com>
2025-03-31 18:53:53 +0300

    Completed

--- src/components/useRequestPost/useRequestPost.jsx
+++ src/components/useRequestPost/useRequestPost.jsx
@@ -9,9 +9,7 @@ export const useRequestPost = (setIsUpdating) => {
     setErrorMessage('')
   }
 
-  const handleAddTask = (event) => {
-    event.preventDefault()
-
+  const templateForAddingTask = () => {
     if (!taskValue) {
       setErrorMessage('Невозможно добавить пустую задачу')
       return
@@ -25,32 +23,20 @@ export const useRequestPost = (setIsUpdating) => {
         completed: false,
       }),
     })
-      .then((rowResponse) => rowResponse.json())
-      .finally(() => {
-        setTaskValue('')
-        setIsUpdating(false)
-      })
+        .then((rowResponse) => rowResponse.json())
+        .finally(() => {
+          setTaskValue('')
+          setIsUpdating(false)
+        })
+  }
+
+  const handleAddTask = (event) => {
+    event.preventDefault()
+    templateForAddingTask()
   }
 
   const addNewTask = () => {
-    if (!taskValue) {
-      setErrorMessage('Невозможно добавить пустую задачу')
-      return
-    }
-
-    fetch('http://localhost:3000/notes', {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json; charset=utf-8' },
-      body: JSON.stringify({
-        title: taskValue.charAt(0).toUpperCase() + taskValue.slice(1),
-        completed: false,
-      }),
-    })
-      .then((rowResponse) => rowResponse.json())
-      .finally(() => {
-        setTaskValue('')
-        setIsUpdating(false)
-      })
+    templateForAddingTask()
   }
 
   return {
@@ -61,56 +47,3 @@ export const useRequestPost = (setIsUpdating) => {
     errorMessage,
   }
 }
-
-
-
-// import { useState } from 'react';
-//
-// export const useRequestPost = (setIsUpdating) => {
-//   const [taskValue, setTaskValue] = useState('');
-//   const [errorMessage, setErrorMessage] = useState('');
-//
-//   const handleInputChange = (event) => {
-//     setTaskValue(event.target.value);
-//     setErrorMessage('');
-//   };
-//
-//   const handleAddTask = async (event) => {
-//     event.preventDefault();
-//
-//     if (!taskValue) {
-//       setErrorMessage('Невозможно добавить пустую задачу');
-//       return;
-//     }
-//
-//     try {
-//       const response = await fetch('http://localhost:3000/notes', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json; charset=utf-8' },
-//         body: JSON.stringify({
-//           title: taskValue.charAt(0).toUpperCase() + taskValue.slice(1),
-//           completed: false,
-//         }),
-//       });
-//
-//       if (!response.ok) {
-//         throw new Error('Ошибка при добавлении задачи');
-//       }
-//
-//       await response.json();
-//     } catch (error) {
-//       console.error(error);
-//       setErrorMessage(error.message);
-//     } finally {
-//       setTaskValue('');
-//       setIsUpdating(true); // Убедитесь, что состояние обновления установлено в true
-//     }
-//   };
-//
-//   return {
-//     taskValue,
-//     handleInputChange,
-//     handleAddTask,
-//     errorMessage,
-//   };
-// };
