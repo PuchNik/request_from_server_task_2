f5b78c2837aa7f7afbdce5bc2022aa841e522ec8
PuchNik <i.p.chn.k@gmail.com>
2025-04-03 19:22:33 +0300

    Рефактор кода

--- src/components/useRequestDelete/useRequestDelete.jsx
+++ src/components/useRequestDelete/useRequestDelete.jsx
@@ -1,4 +1,4 @@
-export const useRequestDelete = (notes, setNotes) => {
+export const useRequestDelete = (setNotes) => {
     const deleteTask = (id) => {
         fetch(`http://localhost:3000/notes/${id}`, {
             method: 'DELETE'
@@ -6,8 +6,8 @@ export const useRequestDelete = (notes, setNotes) => {
             .then(() => {
                 setNotes((prevNotes) => prevNotes.filter(note => note.id !== id));
             })
-
     }
+
     return {
         deleteTask
     }
--- src/components/useRequestPost/useRequestPost.jsx
+++ src/components/useRequestPost/useRequestPost.jsx
@@ -1,53 +1,55 @@
 import {useState} from "react";
 
-export const useRequestPost = (notes, setNotes) => {
-  const [taskValue, setTaskValue] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+export const useRequestPost = (setNotes) => {
+    const [taskValue, setTaskValue] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
 
-  const handleInputChange = (event) => {
-    setTaskValue(event.target.value)
-    setErrorMessage('')
-  }
-
-  const templateForAddingTask = () => {
-    if (!taskValue) {
-      setErrorMessage('Невозможно добавить пустую задачу')
-      setTimeout(() => {
+    const handleInputChange = (event) => {
+        setTaskValue(event.target.value)
         setErrorMessage('')
-      }, 2500)
-      return
     }
 
-    fetch('http://localhost:3000/notes', {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json; charset=utf-8' },
-      body: JSON.stringify({
-        title: taskValue.charAt(0).toUpperCase() + taskValue.slice(1),
-        completed: false,
-      }),
-    })
-        .then(rowResponse => rowResponse.json())
-        .then(newNote => {setNotes((prevNotes) => [...prevNotes, newNote])})
+    const templateForAddingTask = () => {
+        if (!taskValue) {
+            setErrorMessage('Невозможно добавить пустую задачу')
+            setTimeout(() => {
+                setErrorMessage('')
+            }, 2500)
+            return
+        }
 
-        .finally(() => {
-          setTaskValue('')
+        fetch('http://localhost:3000/notes', {
+            method: 'POST',
+            headers: {'Content-type': 'application/json; charset=utf-8'},
+            body: JSON.stringify({
+                title: taskValue.charAt(0).toUpperCase() + taskValue.slice(1),
+                completed: false,
+            }),
         })
-  }
+            .then(rowResponse => rowResponse.json())
+            .then(newNote => {
+                setNotes((prevNotes) => [...prevNotes, newNote])
+            })
 
-  const handleAddTask = (event) => {
-    event.preventDefault()
-    templateForAddingTask()
-  }
+            .finally(() => {
+                setTaskValue('')
+            })
+    }
 
-  const addNewTask = () => {
-    templateForAddingTask()
-  }
+    const handleAddTask = (event) => {
+        event.preventDefault()
+        templateForAddingTask()
+    }
 
-  return {
-    addNewTask,
-    taskValue,
-    handleInputChange,
-    handleAddTask,
-    errorMessage,
-  }
+    const addNewTask = () => {
+        templateForAddingTask()
+    }
+
+    return {
+        addNewTask,
+        taskValue,
+        handleInputChange,
+        handleAddTask,
+        errorMessage,
+    }
 }
